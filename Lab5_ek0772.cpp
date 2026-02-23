2f0d99e6d1994e5a5928da22fbe93bc8cd561653
Elijah <elijahk0505@gmail.com>
2026-02-22 22:17:51 -0500

    Final commit

---
+++ Lab5_ek0772.cpp
@@ -0,0 +1,83 @@
+/**
+ *@file Lab5_username.cpp
+ *@author Elijah K
+ *@date 2025-02-21
+ *@brief A modular program to generate a multiplication table using functions. 
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+int input, maxDigit, shiftingnum;
+
+
+void printInputValidationError() {
+/**
+ * @brief: Error output for invalid input.
+ * @param: None.
+ * @return: None (void).
+ */
+cout << "Invalid input. The digit must be greater than 4 and less than 10 " << endl;
+
+
+
+}
+bool isMaxDigitInputValid(int input) {
+/**
+ * @brief: Validates that user input is within the acceptable range
+ * @param: input - The user-provided integer to validate.
+ * @return: true if the input falls within the strict exclusive range (4,10)
+ */
+return (input > 4 && input < 10);
+}
+
+int getMaxDigitInput() {
+    /**
+ * @brief: Prompts user for input and ensures it is valid.
+ * @param: None.
+ * @return: A validated int representing the maximum digit.
+ */
+cout << "Enter the maximum digit for the multiplication table: " << endl;
+cin >> input;
+
+while (!isMaxDigitInputValid(input)) {
+    printInputValidationError();
+    cin >> input;
+}
+return input;
+}
+void printMultiplicationTable(int maxDigit) {
+/**
+ * @brief: Prints the formatted multiplication table.
+ * @param: maxDigit - The highest digit to include in the table.
+ * @return: None (void).
+ */
+int shiftingnum = 1;
+while (shiftingnum < maxDigit+1) {
+    cout << shiftingnum * 1 << "\t"
+    << shiftingnum * 2 << "\t" 
+    << shiftingnum * 3 << "\t" 
+    << shiftingnum * 4 << "\t" 
+    << shiftingnum * 5 << "\t" 
+    << shiftingnum * 6 << "\t" 
+    << shiftingnum * 7 << "\t" 
+    << shiftingnum * 8 << "\t" 
+    << shiftingnum * 9;
+    cout << endl;
+    shiftingnum++;
+}
+
+} 
+
+int main() {
+/**
+ * @brief: Entry point of the program.
+ * @param: None.
+ * @return: 0 to indicate success
+ */
+int input;
+int maxDigit = getMaxDigitInput();
+isMaxDigitInputValid(input);
+printMultiplicationTable(maxDigit);
+return 0;
+}
